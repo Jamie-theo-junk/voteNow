49a65c1c3a67db3c8ac9a7177558c1732e7b2cf7
jamie junk <jamiejunk27@outlook.com>
2025-02-16 17:35:40 +0200

    jamie#2

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -10,7 +10,7 @@ export default function TabLayout() {
   return (
    <Stack screenOptions={{headerShown: false}}>
     <Stack.Screen name="index" />
-    <Stack.Screen name="explore"/>
+    <Stack.Screen name="home-page"/>
    </Stack>
   );
 }
